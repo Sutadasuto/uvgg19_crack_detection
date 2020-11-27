89500de961de4e82156cd8e72c20f1448bdd2b08
Sutadasuto <rodrigo.rill.g@gmail.com>
2020-11-27 23:35:23 +0100

    Untested version

--- callbacks_and_losses/custom_losses.py
+++ callbacks_and_losses/custom_losses.py
@@ -4,7 +4,6 @@ import tensorflow.keras.backend as K
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.losses import BinaryCrossentropy, MeanSquaredError
-from callbacks_and_losses.leung_malik import LeungMalik
 
 
 # Smooth factor for dice coefficient. DC = (2 * GT n Pred + 1) / (GT u Pred + 1)
@@ -31,44 +30,3 @@ def bce_dsc_loss(alpha=0.5):
 
     return hybrid_loss
 
-
-class LM_Loss(object):
-    def __init__(self):
-        self.leung_malik = LeungMalik()
-        self.filter_bank = self.leung_malik.f
-        self.lm_model = Sequential(layers=Conv2D(self.filter_bank.shape[-1],
-                                         (self.filter_bank.shape[0], self.filter_bank.shape[1]),
-                                         padding="same",
-                                         activation="linear",
-                                         use_bias=False,
-                                         kernel_initializer=self.kernel_initializer,
-                                         data_format="channels_last",
-                                         input_shape=(None, None, 1)
-                                         ))
-        for layer in self.lm_model.layers:
-            layer.trainable = False
-        self.lm_model.build()
-        self.LossFunction = MeanSquaredError()
-        # self.LossFunction = CosineSimilarity()
-
-    def kernel_initializer(self, input_shape, dtype=None):
-        return np.expand_dims(self.leung_malik.make_filters().astype(np.float32), axis=2)
-
-    def loss(self, y_true, y_pred):
-        y_true = self.lm_model(y_true)
-        y_pred = self.lm_model(y_pred)
-        return self.LossFunction(y_true, y_pred)
-
-# lm_loss = LM_Loss()
-
-# def bce_kl_loss(model):
-#     def kl_reconstruction_loss(y_true, y_pred):
-#         # Reconstruction loss
-#         reconstruction_loss = binary_crossentropy(K.flatten(y_true), K.flatten(y_pred)) * model.input_shape[1] * model.input_shape[2]
-#         # KL divergence loss
-#         kl_loss = 1 + model.get_layer("latent_log_sigma").output - K.square(model.get_layer("latent_mu").output) - K.exp(model.get_layer("latent_log_sigma").output)
-#         kl_loss = K.sum(kl_loss, axis=-1)
-#         kl_loss *= -0.5
-#         # Total loss = 50% rec + 50% KL divergence loss
-#         return K.mean(reconstruction_loss + kl_loss)
-#     return kl_reconstruction_loss
